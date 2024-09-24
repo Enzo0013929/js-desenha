d10ac1a9665cd4a1411c6ec376f2c08f171e5d88
Enzo0013929 <enzolimaflorencio@gmail.com>
2024-09-24 19:20:39 -0300

    mudadno o js

--- sketch.js
+++ sketch.js
@@ -1,43 +1,14 @@
-let cor;
- let x;  //horizontal
-let y;  //vertical
-
 function setup() {
-  createCanvas(400, 400);
-  background(100, 0, 0);
-  cor=color(random(0, 255), random(0, 255), random(0, 255));
-  x = 200;
-  y = 200;
- 
-  
-  
+  createCanvas(600, 600);
+  background("green");
 }
-//numeros em circle dizem tamanho e onde "x y" e por último "raio" 
+
 function draw() {
-   circle(x, y, 40);
-  stroke ("yellow");
-  fill (cor);
+  stroke("darkblue");
+  fill("yellow");
 
 
-//o circulo segue o mouse
-
-if(mouseX<x){
-  x = x - 1;
-}
-
-if(mouseX>x){
-  x = x + 1;
-}
-if(mouseY<y){
-  y = y - 1
-}
-if(mouseY>y){
-  y = y + 1;
- }
-// (rgba) com numeros aleatórios o torna transparente
-
-if(mouseIsPressed) {
-    cor = color(random(0, 255), random(0, 255), random(0,255), random(0, 100));
-}
-
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
 }
